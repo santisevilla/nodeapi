1062d45063d743b1284f48390715ce09bc5406e1
Santiago Sevilla <santisevilla.9@gmail.com>
2023-04-17 19:34:05 -0300

    db

---
+++ models/Product.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Product = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter a product name"],
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProductModel = mongoose.model("ProductModel", Product);
+
+module.exports = ProductModel;
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/Product.js");
+
 const app = express();
+
+app.use(express.json());
 
 //routes
 app.get("/", (req, res) => {
   res.send("Maincra");
+});
+
+app.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
