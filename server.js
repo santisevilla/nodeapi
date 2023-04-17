d753dd0de082274e3e850e3af8fdbd2ec6fcd9b5
Santiago Sevilla <santisevilla.9@gmail.com>
2023-04-17 19:42:20 -0300

    id

--- server.js
+++ server.js
@@ -11,6 +11,25 @@ app.get("/", (req, res) => {
   res.send("Maincra");
 });
 
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 app.post("/product", async (req, res) => {
   try {
     const product = await Product.create(req.body);
