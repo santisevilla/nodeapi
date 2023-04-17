80c017887eb15960ec277c8efc51f3eb8245731b
Santiago Sevilla <santisevilla.9@gmail.com>
2023-04-17 20:01:04 -0300

    delete

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const Product = require("./models/Product.js");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //routes
 app.get("/", (req, res) => {
@@ -36,6 +37,37 @@ app.post("/product", async (req, res) => {
     res.status(200).json(product);
   } catch (error) {
     console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `Cannot find any product with ID ${id}` });
+    }
+    const updateProduct = await Product.findById(id);
+    res.status(200).json(updateProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `Cannot find any product with ID ${id}` });
+    }
+    res.status(200).json(product);
+  } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
